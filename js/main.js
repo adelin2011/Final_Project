d586394a6018876dda68ee98f75409c17f7a0fda
Arianna Delin <arianna.delin11@ufl.edu>
2015-12-03 22:07:18 -0500

    updated js file. Changed backstretch plugin to one image instead of three rotating images

--- js/main.js
+++ js/main.js
@@ -1,17 +1,11 @@
 $(document).ready(function(){
-	 $(".background").backstretch(
-	 	[
-	 		"img/paris1.jpg",
-	 		"img/paris2.jpg",
-	 		"img/paris3.jpg"
-	 	], {
-	 		duration: 3000,
-	 		fade: 750});	
+	 $(".background").backstretch("img/paris2.jpg");	
 });
 
 $('#container').flowtype({
- minimum   : 300,
+ minimum   : 500,
  maximum   : 1200,
  minFont   : 15,
  maxFont   : 20,
+ fontRatio : 20,
 });
